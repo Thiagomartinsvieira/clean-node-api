41f8948dcb030878d298093b211f87fa32fb76c8
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-02-18 20:24:00 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,8 +5,7 @@ describe("Signup Controller", () => {
     const sut = new SignUpCOntroller();
     const httprequest = {
       body: {
-        // name: "any_name",
-        email: "any_email@email,com",
+        email: "any_email@email.com",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
@@ -14,5 +13,19 @@ describe("Signup Controller", () => {
     const httpResponse = sut.handle(httprequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Missingg param: name"));
+  });
+
+  test("Should return 400 if no email is provied", () => {
+    const sut = new SignUpCOntroller();
+    const httprequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httprequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missingg param: email"));
   });
 });
