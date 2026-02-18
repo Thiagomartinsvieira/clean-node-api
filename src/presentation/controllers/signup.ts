0bc0c8d58a9ce59de079fd5782a0ec4de76f8ce3
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-02-18 20:23:43 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpCOntroller {
   handle(httprequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("Missingg param: name"),
-    };
+    if (!httprequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missingg param: name"),
+      };
+    }
+
+    if (!httprequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missingg param: email"),
+      };
+    }
   }
 }
